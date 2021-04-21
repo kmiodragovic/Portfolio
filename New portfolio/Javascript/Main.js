6e131a0f7f948a9632ec5125b2d09c2d270b86a9
Kristian <70714035+kmiodragovic@users.noreply.github.com>
2021-04-21 20:44:28 +0200

    Add files via upload

---
+++ New portfolio/Javascript/Main.js
@@ -0,0 +1,20 @@
+const button = document.querySelector(".light-off");
+console.log(button);
+let changeHandler = function () {
+    const elements = document.querySelectorAll(
+        "body, h1, h2, h3, h4, .normal-text"
+    );
+    for (let i = 0; i < elements.length; i++);{
+        const element = elements[i];
+        element.classList.toggle("theme-dark");
+    }
+};
+
+
+function myFunction() {
+    const element = document.querySelector(".card-body");
+    element.classList.toggle("dark-mode");
+ }
+
+
+
